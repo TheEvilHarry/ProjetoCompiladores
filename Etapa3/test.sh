6aed25f2b29c2b7da38e5d235e922b8c61028f79
Pedro Weber Mendonça <pedroweberm@gmail.com>
2021-03-16 16:48:58 -0300

    🐛 Using valgrind

--- Etapa3/test.sh
+++ Etapa3/test.sh
@@ -3,6 +3,6 @@ make clean
 echo "Compiling..."
 make
 echo "Running test from test.txt and saving output to output.txt..."
-./etapa3 <./test.txt #>output.txt
+valgrind --track-origins=yes ./etapa3 <test.txt
 echo "Finished!"
 make clean
