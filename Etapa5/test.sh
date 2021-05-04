da08337f01d47c8b47070839775e2f2e55b67c2a
Pedro Weber Mendonça <pedroweberm@gmail.com>
2021-05-04 19:59:23 -0300

    🐛  Fixed else parser rule

--- Etapa5/test.sh
+++ Etapa5/test.sh
@@ -5,11 +5,14 @@ make
 echo "Running test from test.txt and saving output to output.txt..."
 if [ "$1" = "debug" ]; then
   ./etapa5 <test.txt
-  #valgrind ./etapa5 <test.txt
 else
-  ./etapa5 <test.txt >output.iloc
-  echo "Running output code with ilocsim..."
-  timeout 3 python3 ./ilocsim.py -s -x <output.iloc >ilocsim_output.txt
+  if [ "$1" = "valgrind" ]; then
+    valgrind ./etapa5 <test.txt
+  else
+    ./etapa5 <test.txt >output.iloc
+    echo "Running output code with ilocsim..."
+    timeout 3 python3 ./ilocsim.py -s -x <output.iloc >ilocsim_output.txt
+  fi
 fi
 echo "Finished!"
 make clean
