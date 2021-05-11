84a03f9b79dea644621d6625ddca8eca19b785eb
Pedro Weber Mendonça <pedroweberm@gmail.com>
2021-05-11 00:58:45 -0300

    🐛 Reverted return position change

--- Etapa6/assembly.c
+++ Etapa6/assembly.c
@@ -1,8 +1,8 @@
 #include "assembly.h"
 #include <string.h>
 
-
-void initialCodePrint() {
+void initialCodePrint()
+{
     printf("\t.file\t\"programa.c\"\n");
     printf("\t.text\n");
 }
@@ -20,152 +20,184 @@ void initialCodePrint() {
 //    }
 //}
 
-generateAssembly(Code *code){
+void generateAssembly(Code *code)
+{
     initialCodePrint();
-//    globalInfoPrint();
+    //    globalInfoPrint();
     printAssembly(code);
+}
 
-    }
-
-void pop(char *reg) {
+void pop(char *reg)
+{
     printf("\tmovl\t(%%rsp), %%%s\n", reg); // Tira da pilha
     printf("\taddq\t$4, %%rsp\n");
 }
 
-void push() {
+void push()
+{
     printf("\tsubq\t$4, %%rsp\n");
     printf("\tmovl\t%%eax, (%%rsp)\n"); // Bota na pilha
 }
 
-void print_binary_op_assembly(char *op) {
+void print_binary_op_assembly(char *op)
+{
     pop("edx");
     pop("eax");
     printf("\t%s\t%%edx, %%eax\n", op); // OP
     push();
 }
 
-void print_division_op_assembly(char *op) {
+void print_division_op_assembly(char *op)
+{
     pop("ebx");
     pop("eax");
-    printf("\tcdq\n"); // Extende sinal
+    printf("\tcdq\n");           // Extende sinal
     printf("\t%s\t%%ebx\n", op); // OP
     push();
 }
 
-int isSpecialRegister(char *address){
+int isSpecialRegister(char *address)
+{
 
-    if(strcmp(address,RSP) == 0 || strcmp(address,RFP) == 0 || strcmp(address, RBSS) == 0 )
+    if (strcmp(address, RSP) == 0 || strcmp(address, RFP) == 0 || strcmp(address, RBSS) == 0)
         return 1;
-    else return 0;
-
-    }
-
-char *registerConversion(char *reg) {
-
-    if(strcmp(reg,RBSS) == 0)
-        return "rip";
-    else if(strcmp(reg,RFP) == 0)
-        return "RBP";
-    else if(strcmp(reg,RSP) == 0)
-        return RSP;
-    else return "";
-
+    else
+        return 0;
 }
 
-void conditionalJumpAssembly(char *jump_cond, char* label) {
+char *registerConversion(char *reg)
+{
+
+    if (strcmp(reg, RBSS) == 0)
+        return "rip";
+    else if (strcmp(reg, RFP) == 0)
+        return "RBP";
+    else if (strcmp(reg, RSP) == 0)
+        return RSP;
+    else
+        return "";
+}
+
+void conditionalJumpAssembly(char *jump_cond, char *label)
+{
     pop("edx");
     pop("eax");
     printf("\tcmpl\t%%edx, %%eax\n");
     printf("\t%s\t.%s\n", jump_cond, label);
 }
 
-void printLoadAiAssembly(Code *c) {
-//TODO:
-    if (isSpecialRegister(c->dest1) == 1) {
-        if (strcmp(c->arg1,RBSS)==0)
-            printf("\tmovl\t%s(%%rip), %%%s\n", var_globl_desloc(c->arg2), registerConversion(c->dest1));
-        else
-            printf("\tmovl\t%s(%%%s), %%%s\n", c->arg2, registerConversion(c->arg1), registerConversion(c->dest1));
-    } else {
-        if (strcmp(c->arg1,RBSS)==0)
-            printf("\tmovl\t%s(%%rip), %%eax\n", var_globl_desloc(c->arg2));
-        else
-            printf("\tmovl\t%s(%%%s), %%eax\n", c->arg2, registerConversion(c->arg1));
-        push_Asm();
-    }
+void printLoadAiAssembly(Code *c)
+{
+    // //TODO:
+    // if (isSpecialRegister(c->dest1) == 1)
+    // {
+    //     if (strcmp(c->arg1, RBSS) == 0)
+    //         printf("\tmovl\t%s(%%rip), %%%s\n", var_globl_desloc(c->arg2), registerConversion(c->dest1));
+    //     else
+    //         printf("\tmovl\t%s(%%%s), %%%s\n", c->arg2, registerConversion(c->arg1), registerConversion(c->dest1));
+    // }
+    // else
+    // {
+    //     if (strcmp(c->arg1, RBSS) == 0)
+    //         printf("\tmovl\t%s(%%rip), %%eax\n", var_globl_desloc(c->arg2));
+    //     else
+    //         printf("\tmovl\t%s(%%%s), %%eax\n", c->arg2, registerConversion(c->arg1));
+    //     push_Asm();
+    // }
 }
 
-void printStoreAiAssembly(Code *c){
-//TODO:
-    pop("eax");
-        // Store
-        if (strcmp(c->dest1,RBSS) ==0)
-            printf("\tmovl\t%%eax, %s(%%rip)\n", var_globl_desloc(c->dest2));
-        else
-            printf("\tmovl\t%%eax, %s(%%%s)\n", c->dest2, registerConversion(c->dest1));
-
-    }
-
-void callFunction_Asm(char* label_fun) {
-//TODO:
-    printf("\tcall\t%s\n", get_function_name(label_fun));
+void printStoreAiAssembly(Code *c)
+{
+    // //TODO:
+    // pop("eax");
+    // // Store
+    // if (strcmp(c->dest1, RBSS) == 0)
+    //     printf("\tmovl\t%%eax, %s(%%rip)\n", var_globl_desloc(c->dest2));
+    // else
+    //     printf("\tmovl\t%%eax, %s(%%%s)\n", c->dest2, registerConversion(c->dest1));
 }
 
-void intOperation(char *op, Code *c) {
-    if (strcmp(c->arg1, c->dest1) == 0 && isSpecialRegister(c->arg1)==1) { // Sendo utilizado para inicio de funcao
+void callFunction_Asm(char *label_fun)
+{
+    //TODO:
+    // printf("\tcall\t%s\n", get_function_name(label_fun));
+}
+
+void intOperation(char *op, Code *c)
+{
+    if (strcmp(c->arg1, c->dest1) == 0 && isSpecialRegister(c->arg1) == 1)
+    { // Sendo utilizado para inicio de funcao
         printf("\t%sq\t$%s, %%%s\n", op, c->arg2, registerConversion(c->arg1));
-    } else {
-        if (isSpecialRegister(c->arg1)==1 ) {
+    }
+    else
+    {
+        if (isSpecialRegister(c->arg1) == 1)
+        {
             printf("\tmovl\t%%%s, %%eax\n", registerConversion(c->arg1));
-        } else {
-            pop_Asm("eax");
+        }
+        else
+        {
+            // pop_Asm("eax");
         }
         printf("\t%sl\t$%s, %%eax\n", op, c->arg2);
-        if (isSpecialRegister(c->dest1 )==1) {
+        if (isSpecialRegister(c->dest1) == 1)
+        {
             printf("\tmovl\t%%eax, %%%s\n", registerConversion(c->dest1));
-        } else {
+        }
+        else
+        {
             push();
         }
     }
 }
 
-void printAssembly(Code *c){
-    if (c == NULL) return;
+void printAssembly(Code *c)
+{
+    if (c == NULL)
+        return;
 
-    if (c->label != NULL) {
-    //TODO:
-        if (printa_label_fun(c->label)){ // <========= CHECK THIS LATER
-            // pop_from_return_function = load_parameters(c->label);
-        } else
-            printf(".L%s:\n", c->label);
+    if (c->label != NULL)
+    {
+        //TODO:
+        // if (printa_label_fun(c->label))
+        // { // <========= CHECK THIS LATER
+        // pop_from_return_function = load_parameters(c->label);
+        // }
+        // else
+        printf(".L%s:\n", c->label);
     }
 
-    if (c->type == function_call_preparation_code) {
+    if (c->type == function_call_preparation_code)
+    {
         // Ignora store de RSP e RFP
         printAssembly(c->next->next);
         return;
     }
 
-    if (c->type == function_return_load_code) {
+    if (c->type == function_return_load_code)
+    {
         push();
         printAssembly(c->next);
         return;
     }
 
-    if (c->type == function_return_code) {
+    if (c->type == function_return_code)
+    {
         printf("\tmovl\t(%%rsp), %%eax\n");
         printf("\taddq\t$4, %%rsp\n");
         printAssembly(c->next);
         return;
     }
 
-    if (c->type == main_function_exit_code) {
+    if (c->type == main_function_exit_code)
+    {
         printf("\tleave\n");
         printf("\tret\n");
         return; // Fim do programa - sem nada depois da main
     }
 
-    if (c->type == function_exit_code) {
+    if (c->type == function_exit_code)
+    {
         printf("\tleave\n");
         printf("\tret\n");
         printAssembly(c->next->next->next);
@@ -187,11 +219,13 @@ void printAssembly(Code *c){
         print_division_op_assembly("idivl");
         break;
     case ADDI:
-        if (c->arg1 == RPC){
+        if (c->arg1 == RPC)
+        {
             callFunction_Asm(c->next->next->dest1); // <=========
             c = c->next->next;
-        } else
-            intOperation("add", c);    // <=========
+        }
+        else
+            intOperation("add", c); // <=========
         break;
     case SUBI:
         intOperation("sub", c);
@@ -215,7 +249,7 @@ void printAssembly(Code *c){
         printStoreAiAssembly(c);
         break;
     case I2I:
-        if (isSpecialRegister(c->arg1) && isSpecialRegister(c->dest1)==1) //<====CHECK THIS LATER
+        if (isSpecialRegister(c->arg1) && isSpecialRegister(c->dest1) == 1) //<====CHECK THIS LATER
             printf("\tmovq\t%%%s, %%%s\n", registerConversion(c->arg1), registerConversion(c->dest1));
         break;
     case CMP_LT:
--- Etapa6/symbolTable.c
+++ Etapa6/symbolTable.c
@@ -204,7 +204,7 @@ SymbolTableStack *stackScope()
   newStack->top = NULL;
   newStack->rest = stack;
   newStack->isGlobal = 0;
-  newStack->tableOffset = getGlobalStack()->isGlobal == 1 ? 0 : getGlobalStack()->tableOffset; // Initialize offset with parent table's offset if its unnamed
+  newStack->tableOffset = getGlobalStack()->isGlobal == 1 ? 16 : getGlobalStack()->tableOffset; // Initialize offset with parent table's offset if its unnamed
 
   stack = newStack;
 
